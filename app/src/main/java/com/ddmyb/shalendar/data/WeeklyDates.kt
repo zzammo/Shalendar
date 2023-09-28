5388ca924c253fa4099da96171f7fc84fcdf5874
Dongwon Lee <retomoo@naver.com>
2023-09-29 02:54:06 +0900

    weeklyCalendar updated (uncompleted)

---
+++ app/src/main/java/com/ddmyb/shalendar/data/WeeklyDates.kt
@@ -0,0 +1,12 @@
+package com.ddmyb.shalendar.data
+
+data class WeeklyDates(
+    val month: Int,
+    val sunday: Int,
+    val monday: Int,
+    val tuesday: Int,
+    val wednesday: Int,
+    val thursday: Int,
+    val friday: Int,
+    val saturday: Int
+)
--- app/src/main/java/com/ddmyb/shalendar/view/home/MainActivity.kt
+++ app/src/main/java/com/ddmyb/shalendar/view/home/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.ddmyb.shalendar.view.home
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.ddmyb.shalendar.R
+import com.ddmyb.shalendar.view.weekly.WeeklyCalendarActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.btn_week).setOnClickListener {
+            val intent = Intent(this, WeeklyCalendarActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
