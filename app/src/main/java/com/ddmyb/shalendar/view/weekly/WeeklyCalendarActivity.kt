09b37413e0c6a32cadefc17a4374332b8ef58a9a
Dongwon Lee <retomoo@naver.com>
2023-09-29 01:53:32 +0900

    weeklyCalendar added (uncompleted)

---
+++ app/src/main/java/com/ddmyb/shalendar/view/weekly/WeeklyCalendarActivity.kt
@@ -0,0 +1,11 @@
+package com.ddmyb.shalendar.view.weekly
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WeeklyCalendarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
