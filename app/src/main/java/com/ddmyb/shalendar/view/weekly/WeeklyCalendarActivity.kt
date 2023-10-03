49c2d0603b83065b631b961c76072fa45ea5260c
이동원 <retomoo@naver.com>
2023-10-04 00:44:42 +0900

    HolidayApi added

--- app/src/main/java/com/ddmyb/shalendar/view/weekly/WeeklyCalendarActivity.kt
+++ app/src/main/java/com/ddmyb/shalendar/view/weekly/WeeklyCalendarActivity.kt
@@ -7,10 +7,12 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.ddmyb.shalendar.R
-import com.ddmyb.shalendar.view.weekly.data.WeeklyDates
 import com.ddmyb.shalendar.databinding.ActivityWeeklyCalendarBinding
+import com.ddmyb.shalendar.view.holiday.HolidayApiExplorer
 import com.ddmyb.shalendar.view.weekly.adapter.WeeklyCalendarAdapter
-import java.time.LocalDate
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.util.Calendar
 
 class WeeklyCalendarActivity : AppCompatActivity() {
@@ -25,6 +27,19 @@ class WeeklyCalendarActivity : AppCompatActivity() {
 
         binding.pager.adapter = WeeklyCalendarAdapter(getFirstDays(10),this)
         binding.pager.setCurrentItem(10, false)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val cal = Calendar.getInstance()
+            val names = arrayListOf<String>()
+            val dates = arrayListOf<Long>()
+            HolidayApiExplorer.getHolidays(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH)+1, names, dates)
+            Log.d(TAG, "holiday size: "+names.size)
+            for (i in 0 until names.size) {
+                val caltemp = Calendar.getInstance()
+                cal.timeInMillis = dates[i]
+                Log.d(TAG, "holiday " + names[i] + " " + caltemp.get(Calendar.YEAR) + caltemp.get(Calendar.MONTH)+1 + caltemp.get(Calendar.DATE))
+            }
+        }
     }
 
     fun getFirstDays(radius: Int): MutableList<Long> {
