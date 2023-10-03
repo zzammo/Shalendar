6082038d293448a7f715f8e57974ef9a4a546f5d
이동원 <retomoo@naver.com>
2023-10-03 23:29:59 +0900

    weeklyCalendar updated (DB, Dialog needed)

--- app/src/main/java/com/ddmyb/shalendar/view/schedules/utils/StartDateTimeDto.kt
+++ app/src/main/java/com/ddmyb/shalendar/view/schedules/utils/StartDateTimeDto.kt
@@ -1,8 +1,9 @@
 package com.ddmyb.shalendar.view.schedules.utils
 
+import java.io.Serializable
 import java.time.LocalDateTime
 
 data class StartDateTimeDto(
     val scheduleId: String?,
     val dateTime: LocalDateTime
-)
+):Serializable
