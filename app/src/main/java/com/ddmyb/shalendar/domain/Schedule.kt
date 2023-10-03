b6f2b8c3b5a0ea49a6cf75ae14f3b82bc3f9a718
jaemoyang <didwoah7@gmail.com>
2023-10-03 18:52:33 +0900

    schedule 5

--- app/src/main/java/com/ddmyb/shalendar/domain/Schedule.kt
+++ app/src/main/java/com/ddmyb/shalendar/domain/Schedule.kt
@@ -1,36 +1,23 @@
 package com.ddmyb.shalendar.domain
 
 import android.location.Location
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.ddmyb.shalendar.view.schedules.utils.MeansType
 import com.google.android.gms.maps.model.LatLng
 import java.time.LocalDateTime
 
+@RequiresApi(Build.VERSION_CODES.O)
 data class Schedule (
     private var scheduleId: String? = null,
 
     var startLocalDateTime: LocalDateTime? = null,
-
-    // 일정 시작 정보
-    var startHour: Int = 8,
-    var startMinute: Int = 0,
-    var startMonth: Int = 1,
-    var startDay: Int = 1,
-    var startYear: Int = 2023,
-    var startWeek: Int = 0,
-
-    // 일정이 끝나는 정보
-    var endHour: Int = 9,
-    var endMinute: Int = 0,
-    var endMonth: Int = 1,
-    var endDay: Int = 1,
-    var endYear: Int = 2023,
-    var endWeek: Int = 0,
+    var endLocalDateTime: LocalDateTime? = null,
 
     // 소요 시간
     var meansType: MeansType = MeansType.NULL,
-    var costHour: Int = 0,
     var costMinute: Int = 0,
-    var costSecond: Int = 0,
+    var costText: String? = null,
 
     // 출발 위치 + 도착 위치
     var srcLocation: Location? = null,
@@ -40,12 +27,8 @@ data class Schedule (
     var srcAddress: String? = null,
     var dstAddress: String? = null,
 
-    // 출발 예정 시각 : 년 월 일 시간 분
-    var expectedStartYear: Int = 0,
-    var expectedStartHour: Int = 0,
-    var expectedStartMinute: Int = 0,
-    var expectedStartMonth: Int = 0,
-    var expectedStartDay: Int = 0,
+    // 출발 예정 시간
+    var departureLocalDateTime: LocalDateTime? = null,
 
     var alarm: String? = null,
     var memo: String? = null
---
+++ app/src/main/java/com/ddmyb/shalendar/view/schedules/utils/StartDateTimeDto.kt
@@ -0,0 +1,8 @@
+package com.ddmyb.shalendar.view.schedules.utils
+
+import java.time.LocalDateTime
+
+data class StartDateTimeDto(
+    val scheduleId: String,
+    val dateTime: LocalDateTime
+)
