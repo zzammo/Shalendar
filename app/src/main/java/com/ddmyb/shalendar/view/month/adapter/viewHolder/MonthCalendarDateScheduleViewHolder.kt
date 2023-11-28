19c7548df5c22ad85313b9f84690f7d0b66c631a
Dirtfy <irsam78@gmail.com>
2023-11-28 21:52:30 +0900

    monthCalendar group schedule color edit

--- app/src/main/java/com/ddmyb/shalendar/view/month/adapter/viewHolder/MonthCalendarDateScheduleViewHolder.kt
+++ app/src/main/java/com/ddmyb/shalendar/view/month/adapter/viewHolder/MonthCalendarDateScheduleViewHolder.kt
@@ -6,16 +6,27 @@ import androidx.recyclerview.widget.RecyclerView
 import com.ddmyb.shalendar.R
 import com.ddmyb.shalendar.databinding.ItemMonthScheduleBinding
 import com.ddmyb.shalendar.domain.schedules.repository.ScheduleDto
+import com.ddmyb.shalendar.domain.users.UserRepository
 
 class MonthCalendarDateScheduleViewHolder(
     val binding: ItemMonthScheduleBinding
 ): RecyclerView.ViewHolder(binding.root){
 
     fun bind(schedule: ScheduleDto) {
-        binding.schedule = schedule
+
         val drawable = ContextCompat.getDrawable(binding.root.context, R.drawable.weekly_holiday) as GradientDrawable
 //        drawable.mutate()
-        drawable.setColor(ContextCompat.getColor(binding.root.context, schedule.color))
+        if (schedule.groupId != "")
+            drawable.setColor(ContextCompat.getColor(binding.root.context, R.color.google_blue))
+        else if (schedule.userId == UserRepository().getUserId())
+            drawable.setColor(ContextCompat.getColor(binding.root.context, schedule.color))
+        else {
+            drawable.setColor(ContextCompat.getColor(binding.root.context, R.color.line_gray))
+            schedule.title = ""
+        }
+
+        binding.schedule = schedule
+
         binding.scheduleName.background = drawable
     }
 }
