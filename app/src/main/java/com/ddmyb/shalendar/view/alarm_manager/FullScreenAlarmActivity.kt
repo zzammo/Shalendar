5174a5462bc754c731f5240cf4630b55db9d4bf5
jaemoyang <didwoah7@gmail.com>
2023-11-28 21:01:32 +0900

    full screen notification fix

--- app/src/main/java/com/ddmyb/shalendar/view/alarm_manager/FullScreenAlarmActivity.kt
+++ app/src/main/java/com/ddmyb/shalendar/view/alarm_manager/FullScreenAlarmActivity.kt
@@ -2,6 +2,7 @@ package com.ddmyb.shalendar.view.alarm_manager
 
 import android.annotation.SuppressLint
 import android.app.KeyguardManager
+import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
@@ -26,56 +27,26 @@ class FullScreenAlarmActivity : AppCompatActivity() {
 
         binding = ActivityFullScreenAlarmBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         turnScreenOnAndKeyguardOff()
 
-        binding.dummyButton.setOnClickListener {
+        binding.btnAlarmCancel.setOnClickListener {
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.cancelAll()
             finish()
         }
     }
 
     private fun turnScreenOnAndKeyguardOff(){
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
-//            setShowWhenLocked(true)
-//            setTurnScreenOn(true)
-//            window.addFlags(
-//                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
-//                    or WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON)
-//        } else {
-//            window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED    // deprecated api 27
-//                    or WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD     // deprecated api 26
-//                    or WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
-//                    or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON   // deprecated api 27
-//                    or WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON)
-//        }
-//        val keyguardMgr = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            keyguardMgr.requestDismissKeyguard(this@FullScreenAlarmActivity, null)
-//        }
-        // 안드12 이상에서 잠금화면 위로 액티비티 띄우기 & 화면 켜기
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
-//            setShowWhenLocked(true)
-//            setTurnScreenOn(true)
-//            (getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager).apply {
-//                requestDismissKeyguard(this@FullScreenAlarmActivity, null)
-//            }
-//        } else {
-//            this.window.addFlags(
-//                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
-//                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
-//                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
-//            )
-//        }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
             Log.d("didwoah","version high")
             setShowWhenLocked(true)
             setTurnScreenOn(true)
-            window.addFlags(
-                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
-                    or WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON)
-            val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
-            keyguardManager.requestDismissKeyguard(this, null)
+            window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                    or WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                    or WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON
+                    or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+            )
         } else {
             Log.d("didwoah","version low")
             window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED    // deprecated api 27
@@ -84,6 +55,8 @@ class FullScreenAlarmActivity : AppCompatActivity() {
                     or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON   // deprecated api 27
                     or WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON)
         }
+        val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+        keyguardManager.requestDismissKeyguard(this, null)
     }
 
     companion object {
